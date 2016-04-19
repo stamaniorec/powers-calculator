98fdb5d7df3e1e3286765e4ebefa712b35ee5fc9
stamaniorec <stamaniorec@gmail.com>
2016-04-19 20:03:29 +0300

    Made string_t a little more C-like by using pointers for iteration instead of a for.

--- string_t.c
+++ string_t.c
@@ -6,40 +6,21 @@
 void init_string(string_t* string, char* initial_value) {
     int length = strlen(initial_value);
     string->length = length;
-    // string->capacity = length * 2;
-    // string->string = malloc(sizeof(char) * length);
 
-
-    int i;
-    // make me better
-    for(i = 0; i < length; ++i) {
+    int i = 0;
+    while(initial_value[i] != '\0') {
         string->string[i] = initial_value[i];
+        ++i;
     }
-    string->string[length] = '\0';
+    string->string[i] = '\0';
 }
 
-// void resize_if_needed(string_t* string, char* value) {
-//     int length = strlen(value);
-//     int new_length = string->length + length;
-//     if(string->capacity < new_length) {
-//         int new_capacity = new_length * 2;
-//         char* new_buffer = malloc(sizeof(char) * new_capacity);
-//         strcpy(new_buffer, string->string);
-//         free(string->string);
-//         string->string = new_buffer;
-//         string->capacity = new_capacity;
-//     }
-// }
-
 void append(string_t* string, char* value) {
-    // resize_if_needed(string, value);
-    // int new_length = string->length + strlen(value);
-    string->length = string->length + strlen(value); //new_length;
+    string->length = string->length + strlen(value);
     strcat(string->string, value);
 }
 
 void prepend(string_t* string, char* value) {
-    // resize_if_needed(string, value);
     int length = strlen(value);
     int i;
     for(i = string->length-1; i >= 0; --i) {
@@ -51,6 +32,4 @@ void prepend(string_t* string, char* value) {
 }
 
 void destroy_string(string_t* string)
-{
-    // free(string->string);
-}
+{}
